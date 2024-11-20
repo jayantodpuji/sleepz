e3949e797c01aa93a8465eeeef1eac6b7258a6b6
jayantodpuji <dedypuji@outlook.com>
2024-11-20 13:17:19 +0700

    test: add more test to user_actions controller

--- spec/requests/api/v1/user_actions_spec.rb
+++ spec/requests/api/v1/user_actions_spec.rb
@@ -12,5 +12,37 @@ RSpec.describe "Api::V1::UserActionsController", type: :request do
         expect(JSON.parse(response.body).length).to eq(1)
       end
     end
+
+    context "when the user already has previous actions" do
+      before do
+        user.user_actions.create!(action: "sleep", action_time: Time.current - 24.hour)
+        user.user_actions.create!(action: "awake", action_time: Time.current - 23.hour)
+      end
+
+      it "creates a new user action if it is a valid transition" do
+        post "/api/v1/user_actions", params: { user_id: user.id, user_action: "sleep", user_action_time: Time.current }
+
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body).length).to eq(3)
+      end
+
+      it "returns an error if the last action is the same as the new action" do
+        post "/api/v1/user_actions", params: { user_id: user.id, user_action: "awake", user_action_time: Time.current }
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq({ "error" => "Invalid action. Last action was also awake" })
+      end
+
+      it "returns actions ordered by created_at ascending after creating a new action" do
+        post "/api/v1/user_actions", params: { user_id: user.id, user_action: "sleep", user_action_time: Time.current }
+
+        expect(response).to have_http_status(:ok)
+
+        user_actions = JSON.parse(response.body)
+
+        created_at_times = user_actions.map { |action| action["created_at"] }
+        expect(created_at_times).to eq(created_at_times.sort)
+      end
+    end
   end
 end
