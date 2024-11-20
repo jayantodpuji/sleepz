aae3c9732178627f897ffb1553e94acac691a90f
jayantodpuji <dedypuji@outlook.com>
2024-11-20 16:17:42 +0700

    fix: calculate duration_in_second in before_save callback

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -2,4 +2,10 @@ class SleepRecord < ApplicationRecord
   belongs_to :user
 
   validates :sleep_time, presence: true
+
+  before_save :calculate_duration_in_seconds
+
+  private def calculate_duration_in_seconds
+    self.duration_in_second = (wake_time - sleep_time).to_i if sleep_time.present? && wake_time.present?
+  end
 end
--- spec/models/sleep_record_spec.rb
+++ spec/models/sleep_record_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe SleepRecord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { User.create!(name: "Verde") }
+
+  context "with valid attributes" do
+    it "produces valid record" do
+      sleep_record = SleepRecord.new(user: user, sleep_time: Time.current, wake_time: Time.current)
+      expect(sleep_record.valid?).to eq(true)
+    end
+
+    it "produces valid record without wake_time" do
+      sleep_record = SleepRecord.new(user: user, sleep_time: Time.current)
+      expect(sleep_record.valid?).to eq(true)
+    end
+
+    it "produces valid record with duration_in_second" do
+      sleep_record = SleepRecord.new(user: user, wake_time: Time.current, sleep_time: Time.current - 1.hour)
+      expect(sleep_record.valid?).to eq(true)
+
+      sleep_record.save!
+      expect(sleep_record.duration_in_second).to eq(3599) # 1 hour in seconds
+    end
+  end
+
+  context "with invalid sleep_time" do
+    it "produces invalid record" do
+      sleep_record = SleepRecord.new(user: user)
+      expect(sleep_record.valid?).to eq(false)
+    end
+  end
 end
