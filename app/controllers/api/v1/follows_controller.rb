72dd3ddc15cde97bd9fe29e3913ee987489cc339
jayantodpuji <dedypuji@outlook.com>
2024-11-21 12:18:10 +0700

    feat: init create action to register follow record

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -1,6 +1,20 @@
 module Api
   module V1
     class FollowsController < ApplicationController
+      def create
+        follower = User.find(follow_params[:follower_id])
+        followed = User.find(follow_params[:followed_id])
+
+        Follow.create!(follower: follower, followed: followed)
+        head :no_content
+      rescue ActiveRecord::RecordInvalid => e
+        render json: { error: e.message }, status: :unprocessable_entity
+      end
+
+      private def follow_params
+        # TODO: should we use params.require(:follow).permit(:follower_id, :followed_id)? need to research
+        params.permit(:follower_id, :followed_id)
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :user_actions, only: [:create]
+      resources :follows, only: [:create]
     end
   end
 end
