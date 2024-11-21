25b513348e2426a52838b1ae94149fadcc57ff37
jayantodpuji <dedypuji@outlook.com>
2024-11-21 16:52:54 +0700

    chore: init seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,34 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "faker"
+
+puts "Seed data program"
+
+puts "Delete all data"
+Follow.delete_all
+UserAction.delete_all
+SleepRecord.delete_all
+User.delete_all
+
+puts "Seeding started"
+users = 10.times.map { User.create!(name: Faker::Name.unique.name) }
+
+users.each do |current_user|
+  other_users = users.reject { |user| user == current_user }
+
+  other_users.each do |followed_user|
+    Follow.create!(
+      follower_id: current_user.id,
+      followed_id: followed_user.id
+    )
+  end
+end
+
+users.each do |current_user|
+  10.times do
+    sleep_time = Faker::Time.between(from: 1.week.ago, to: Time.current).round
+    awake_time = sleep_time + rand(1..8).hour
+
+    UserAction.create!(user: current_user, action: "sleep", action_time: sleep_time)
+    UserAction.create!(user: current_user, action: "awake", action_time: awake_time)
+    SleepRecord.create!(user: current_user, sleep_time: sleep_time, awake_time: awake_time)
+  end
+end
