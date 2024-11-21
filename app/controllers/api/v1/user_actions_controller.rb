047849515a013c830d97b2b92ff68bd1f0755c7b
jayantodpuji <dedypuji@outlook.com>
2024-11-21 12:14:27 +0700

    chore: change user_actions controller class definition to use module

--- app/controllers/api/v1/user_actions_controller.rb
+++ app/controllers/api/v1/user_actions_controller.rb
@@ -1,20 +1,24 @@
-class Api::V1::UserActionsController < ApplicationController
-  def create
-    current_user = User.find(create_action_params[:user_id])
+module Api
+  module V1
+    class UserActionsController < ApplicationController
+      def create
+        current_user = User.find(create_action_params[:user_id])
 
-    updated_user_actions = UserActionService::RegisterAction.call(
-      current_user,
-      create_action_params[:user_action],
-      create_action_params[:user_action_time]
-    )
-    render json: updated_user_actions, status: :ok
-  rescue UserActionService::InvalidActionError => e
-    render json: { error: e.message }, status: :unprocessable_entity
-  rescue StandardError
-    render json: { error: 'An unexpected error occurred. Please try again later.' }, status: :internal_server_error
-  end
+        updated_user_actions = UserActionService::RegisterAction.call(
+          current_user,
+          create_action_params[:user_action],
+          create_action_params[:user_action_time]
+        )
+        render json: updated_user_actions, status: :ok
+      rescue UserActionService::InvalidActionError => e
+        render json: { error: e.message }, status: :unprocessable_entity
+      rescue StandardError
+        render json: { error: 'An unexpected error occurred. Please try again later.' }, status: :internal_server_error
+      end
 
-  private def create_action_params
-    params.permit(:user_id, :user_action, :user_action_time)
+      private def create_action_params
+        params.permit(:user_id, :user_action, :user_action_time)
+      end
+    end
   end
 end
